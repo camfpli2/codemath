f7cdf2f78d5db213b82f4ad9481527091a26b031
camfpli2 <37838007+camfpli2@users.noreply.github.com>
2025-06-27 17:10:19 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+var cards=[];
+
+function setup(){
+  createCanvas(400,400);
+  background(220);
+}
+
+
+
+class card{
+  constructor(x,y,w,h,txt){
+    this.x=x;this.y=y;this.w=w;this.h=h;this.txt=txt;
+  }
+
+  
+}
